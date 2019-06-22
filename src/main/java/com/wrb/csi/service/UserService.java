64fa84771b24b7e18457e5d25df5493ff788e492
WongLong <1052786220@qq.com>
2019-06-22 10:54:09 +0800

    逆向生成实体

--- src/main/java/com/wrb/csi/service/UserService.java
+++ src/main/java/com/wrb/csi/service/UserService.java
@@ -1,10 +1,6 @@
 package com.wrb.csi.service;
 
-import java.util.List;
 
-import com.wrb.csi.model.User;
 
 public interface UserService {
-   public User selectUserById(Integer userId); 
-   public List<User> findAllUser();
 }
--- src/main/java/com/wrb/csi/service/impl/UserServiceImpl.java
+++ src/main/java/com/wrb/csi/service/impl/UserServiceImpl.java
@@ -8,39 +8,28 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
-import com.wrb.csi.dao.UserDao;
-import com.wrb.csi.model.User;
 import com.wrb.csi.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService{
-    @Autowired  
-    private UserDao userDao;
-    @Autowired
-    private RedisTemplate redisTemplate;
-    
-    @Override
-    public User selectUserById(Integer userId) {
-        return userDao.selectUserById(userId);  
-    }
-
-	@Override
-	public List<User> findAllUser() {
-		String key = "user";
-        ValueOperations<String, List<User>> operations = redisTemplate.opsForValue();
-        
-        boolean hasKey = redisTemplate.hasKey(key);
-        if (hasKey) {
-        	List<User> users = operations.get(key);
-        	System.out.println("从Redis中获取Users");
-        	
-        	return users;
-        }
-        
-        List<User> users = userDao.findAllUser();
-        operations.set(key, users, 10, TimeUnit.SECONDS);
-        System.out.println("将users存入Redis缓存");
-    	return users;
-	}
+	/*
+	 * @Autowired private UserDao userDao;
+	 * 
+	 * @Autowired private RedisTemplate redisTemplate;
+	 * 
+	 * @Override public User selectUserById(Integer userId) { return
+	 * userDao.selectUserById(userId); }
+	 * 
+	 * @Override public List<User> findAllUser() { String key = "user";
+	 * ValueOperations<String, List<User>> operations = redisTemplate.opsForValue();
+	 * 
+	 * boolean hasKey = redisTemplate.hasKey(key); if (hasKey) { List<User> users =
+	 * operations.get(key); System.out.println("从Redis中获取Users");
+	 * 
+	 * return users; }
+	 * 
+	 * List<User> users = userDao.findAllUser(); operations.set(key, users, 10,
+	 * TimeUnit.SECONDS); System.out.println("将users存入Redis缓存"); return users; }
+	 */
 
 }
